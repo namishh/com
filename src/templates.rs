0393a04269601c7e9cae33159f13dadd5f745046
nam <namish@tuta.io>
2026-01-16 21:56:38 +0530

    save

--- src/templates.rs
+++ src/templates.rs
@@ -5,10 +5,11 @@ fn format_score(value: &Value, _: &HashMap<String, Value>) -> TeraResult<Value> 
     match value {
         Value::Number(n) => {
             if let Some(f) = n.as_f64() {
-                if f.fract() == 0.0 {
-                    Ok(Value::String(format!("{}", f as i64)))
+                let rounded = (f * 10.0).round() / 10.0;
+                if rounded.fract() == 0.0 {
+                    Ok(Value::String(format!("{}", rounded as i64)))
                 } else {
-                    Ok(Value::String(format!("{}", f)))
+                    Ok(Value::String(format!("{:.1}", rounded)))
                 }
             } else {
                 Ok(value.clone())
